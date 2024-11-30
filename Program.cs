14c31201808de11194226c0f0116257444d4d4b6
joeybravano <c6162010@gmail.com>
2024-11-29 20:21:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using CC__graphical_framework.Engine.workspace.Base;
+using CC__graphical_framework.Engine.workspace.Graphics;
+
+Window window = new(700, 700, "CC+");
+
+window.Run();
+
+while (window.IsOpen())
+{
+    window.Display();
+    window.DispatchEvents();
+    var gray = CSprite.create(0, 0, "test.png");
+    window.DrawSpriteTS(gray);
+}
